62869467702ef2bc54f0056f1f6f5ef889d66ddc
miludehutu <1010291042@qq.com>
2021-07-05 23:05:26 +0800

    第四次

--- microservice-provider-user/src/main/java/com/microservice/MicroserviceProviderUserApplication.java
+++ microservice-provider-user/src/main/java/com/microservice/MicroserviceProviderUserApplication.java
@@ -3,10 +3,12 @@ package com.microservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @MapperScan("com.microservice.mapper") //扫描的mapper
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceProviderUserApplication {
 
 public static void main(String[] args) {
