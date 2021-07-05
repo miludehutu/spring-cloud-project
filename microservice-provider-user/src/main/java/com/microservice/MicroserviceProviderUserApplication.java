05cf158e2c229e4649d1f738416162d493e57163
miludehutu <1010291042@qq.com>
2021-07-05 21:37:11 +0800

    第二次

--- microservice-provider-user/src/main/java/com/microservice/MicroserviceProviderUserApplication.java
+++ microservice-provider-user/src/main/java/com/microservice/MicroserviceProviderUserApplication.java
@@ -9,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroserviceProviderUserApplication {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         SpringApplication.run(MicroserviceProviderUserApplication.class, args);
-    }
+        }
 
-}
+        }
